0757c7b4350a05d1842dcbcfb7f2ddbca91cd772
Minified <gora.pub@gmail.com>
2017-03-01 00:45:06 +0100

    content

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php print(basename(__DIR__)); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <?php include(__DIR__.'/body.html'); ?>
+    <style><?php include(__DIR__.'/body.css'); ?></style>
+  </body>
+</html>
